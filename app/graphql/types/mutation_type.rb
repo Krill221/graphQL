4525168d83faf8b0ab57f7fc4a80b353bff6963f
Кирилл Еропкин <krill221@gmail.com>
2018-08-15 15:14:45 +0300

    user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
+
+    field :createLink, function: Resolvers::CreateLink.new
+
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def test_field
